784d3136b12c5a313daf7832cd4a8d939881dc2a
Nathan Hilburn <Garrett.nhil@gmail.com>
2022-10-30 17:46:06 -0500

    VR Hands, animations, and controller input

---
+++ Scripts/HandAnimationController.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimationController : MonoBehaviour
+{
+    [SerializeField] InputActionReference controllerActionGrip;
+    [SerializeField] InputActionReference controllerActionTrigger;
+
+    private Animator _handAnimator;
+
+    private void Awake(){
+
+        controllerActionGrip.action.performed += GripPress;
+        controllerActionTrigger.action.performed += TriggerPress;
+        controllerActionGrip.action.canceled += GripCancel;
+        controllerActionTrigger.action.canceled += TriggerCancel;
+        _handAnimator = GetComponent<Animator>();
+    }
+
+    private void GripCancel(InputAction.CallbackContext obj){
+        _handAnimator.SetFloat("Grip", 0);
+    }
+
+    private void TriggerCancel(InputAction.CallbackContext obj){
+        _handAnimator.SetFloat("Trigger", 0);
+    }
+
+    private void GripPress(InputAction.CallbackContext obj){
+        _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+    }
+
+    private void TriggerPress(InputAction.CallbackContext obj){
+        _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+    }
+}
